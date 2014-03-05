55dec57e4ba0cb92f651b8ff0e16aa079a58bd76
eignhpants <ian.cullinane@gmail.com>
2014-03-05 11:22:42 -0500

    Folder Groups Made
    Made folders for the app and web form separately

---
+++ web/index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<HTML lang="en">
+
+<head>
+<meta charset="utf-8">
+<link href="stylesheet.css" rel="stylesheet">
+ <title>Part Time Professor FOrm</title>
+ 
+</head><!-- end head div -->
+
+<body>
+
+<div class="grid">
+	Grid Parent
+	
+
+</div><!-- end grid div -->
+
+
+
+
+<div class="wrapper">
+	<div>
+		<h1> Teacher Availibility Form</h1>
+
+		<p>This is the form you fill out if you want to ever teach here or i will so fuck you up.</p>
+
+	</div>
+
+	<div class="days">
+		<form name="teacherForm" action="submitted.php" method="POST" action="<?php $PHP_SELF; ?>"name=>
+			
+			
+				<legend>Days of the week availability</legend>
+
+				<table>
+				<tr>
+					<td class="day"><p>mon</p></td>					
+					<td class="day"><p>tue</p></td>
+					<td class="day"><p>wed</p></td>
+					<td class="day"><p>thu</p></td>
+					<td class="day"><p>fri</p></td>
+					<td class="day"><p>sat</p></td>
+					<td class="day"><p>sun</p></td>
+				</tr><!-- end row -->
+				<tr>
+					<td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+
+				</tr><!-- end row -->
+				<tr>
+					<td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+
+				</tr><!-- end row -->
+					
+				
+					
+
+				</table>
+
+			
+
+
+		</form>
+	</div><!-- end days div -->
+
+	</br>
+
+	<form></form>
+
+	</br>
+
+	<form></form>
+
+</div><!-- end wrapper class -->
+
+</body>
+
+</html>
